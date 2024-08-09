aa76f30e9e95a0d43d2cd8e61452d35a1e7d5905
Zac Berkowitz <zac.berkowitz@gmail.com>
2024-08-08 22:48:30 -0400

    add: kakoune with KAKOUNE_RUNTIME search path
      This local kakoune package adds a native-search-path to set
      KAKOUNE_RUNTIME to the profile directory.  Otherwise kakoune
      uses the package derivation path and will not see any additional
      plugins.

---
+++ zb/packages/kakoune.scm
@@ -0,0 +1,13 @@
+(define-module (zb packages kakoune)
+  #:use-module (guix packages)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages text-editors))
+
+(define-public zb/kakoune
+  (package
+    (inherit kakoune)
+    ;; (name "kakoune-sp")
+    (native-search-paths
+      (list (search-path-specification
+              (variable "KAKOUNE_RUNTIME")
+              (files (list "share/kak")))))))
