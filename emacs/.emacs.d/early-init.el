43e16ce31df845470f8b28b9c59b2a84f2a7602e
Zac Berkowitz <zac.berkowitz@gmail.com>
2024-09-02 23:44:16 +0000

    add skeleton emacs config

---
+++ emacs/.emacs.d/early-init.el
@@ -0,0 +1,10 @@
+;; -*- lexical-binding: t; -*-
+
+;; elpaca is used for package management and is configured in init.el
+(setq package-enable-at-startup nil)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
